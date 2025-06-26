640e94b60e72498eadc3a781d55302c29abbec87
david-sim0niants <david.simonyants@gmail.com>
2025-06-26 09:48:44 +0400

    Add strides in matmul

--- include/matmul.h
+++ include/matmul.h
@@ -7,7 +7,8 @@
 template<typename T> requires (std::integral<T> || std::floating_point<T>)
 struct Matmul {
     static void matmul(const T *dev_A, const T *dev_B, T *dev_C,
-                       std::size_t N, std::size_t K, std::size_t M);
+                       std::size_t N, std::size_t K, std::size_t M,
+                       std::size_t A_stride, std::size_t B_stride, std::size_t C_stride);
 };
 
 /** Matrix multiplication: C = A x B
@@ -17,5 +18,16 @@ inline void matmul(const T *dev_A, const T *dev_B, T *dev_C,
                    std::size_t N, std::size_t K, std::size_t M)
     requires (std::integral<T> || std::floating_point<T>)
 {
-    return Matmul<T>::matmul(dev_A, dev_B, dev_C, N, K, M);
+    return Matmul<T>::matmul(dev_A, dev_B, dev_C, N, K, M, K, M, M);
+}
+
+/** Matrix multiplication: C = A x B
+ * A: [N x K], B: [K x M], C: [N x M] — all in device memory */
+template<typename T>
+inline void matmul(const T *dev_A, const T *dev_B, T *dev_C,
+                   std::size_t N, std::size_t K, std::size_t M,
+                   std::size_t A_stride, std::size_t B_stride, std::size_t C_stride)
+    requires (std::integral<T> || std::floating_point<T>)
+{
+    return Matmul<T>::matmul(dev_A, dev_B, dev_C, N, K, M, A_stride, B_stride, C_stride);
 }
--- src/matmul.cu
+++ src/matmul.cu
@@ -6,7 +6,8 @@ __global__ void matmul_kernel(
         const T * __restrict__ A,
         const T * __restrict__ B,
         T * __restrict__ C,
-        const size_t N, const size_t K, const size_t M)
+        const size_t N, const size_t K, const size_t M,
+        const size_t A_stride, const size_t B_stride, const size_t C_stride)
 {
     size_t row = blockIdx.y * blockDim.y + threadIdx.y;
     size_t col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -16,9 +17,9 @@ __global__ void matmul_kernel(
 
     T c = 0;
     for (size_t k = 0; k < K; ++k)
-        c += A[row * K + k] * B[k * M + col];
+        c += A[row * A_stride + k] * B[k * B_stride + col];
 
-    C[row * M + col] = c;
+    C[row * C_stride + col] = c;
 }
 
 constexpr size_t TILE_SIZE = 16;
@@ -28,7 +29,8 @@ __global__ void matmul_tiled_kernel(
         const T * __restrict__ A,
         const T * __restrict__ B,
         T * __restrict__ C,
-        const size_t N, const size_t K, const size_t M)
+        const size_t N, const size_t K, const size_t M,
+        const size_t A_stride, const size_t B_stride, const size_t C_stride)
 {
     const size_t row = blockIdx.y * blockDim.y + threadIdx.y;
     const size_t col = blockIdx.x * blockDim.x + threadIdx.x;
@@ -44,10 +46,10 @@ __global__ void matmul_tiled_kernel(
         size_t k = 0;
 
         k = t * TILE_SIZE + j;
-        A_sub[i][j] = row < N && k < K ? A[row * K + k] : T(0); // A[row][k]
+        A_sub[i][j] = row < N && k < K ? A[row * A_stride + k] : T(0); // A[row][k]
 
         k = t * TILE_SIZE + i;
-        B_sub[i][j] = col < M && k < K ? B[k * M + col] : T(0); // B[k][col]
+        B_sub[i][j] = col < M && k < K ? B[k * B_stride + col] : T(0); // B[k][col]
 
         __syncthreads();
 
@@ -58,17 +60,19 @@ __global__ void matmul_tiled_kernel(
     }
 
     if (row < N && col < M)
-        C[row * M + col] = c;
+        C[row * C_stride + col] = c;
 }
 
 template<typename T> requires (std::integral<T> || std::floating_point<T>)
 void Matmul<T>::matmul(
         const T *dev_A, const T *dev_B, T *dev_C,
-        size_t N, size_t K, size_t M)
+        size_t N, size_t K, size_t M,
+        size_t A_stride, size_t B_stride, size_t C_stride)
 {
     dim3 block_size(TILE_SIZE, TILE_SIZE);
     dim3 grid_size((M + block_size.x - 1) / block_size.x, (N + block_size.y - 1) / block_size.y);
-    matmul_tiled_kernel<<<grid_size, block_size>>>(dev_A, dev_B, dev_C, N, K, M);
+    matmul_tiled_kernel<<<grid_size, block_size>>>(dev_A, dev_B, dev_C, N, K, M,
+                                                   A_stride, B_stride, C_stride);
     assert_cuda_ok();
     cudaDeviceSynchronize();
 }
